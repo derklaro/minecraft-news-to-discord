e54ca4a73d6dd83724c2cc4c24e06e53afde89e0
Pasqual Koschmieder <git@derklaro.dev>
2025-11-11 22:13:19 +0100

    cap backoff seconds

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ def to_minecraft_url(path):
 
 
 def create_http_session():
-    retry = Retry(total=10, backoff_factor=2, status_forcelist=[429, 500, 502, 503, 504],
+    retry = Retry(total=10, backoff_factor=2, backoff_max=10, status_forcelist=[429, 500, 502, 503, 504],
                   allowed_methods=["GET", "POST"], respect_retry_after_header=True)
     adapter = HTTPAdapter(max_retries=retry)
     session = requests.Session()
